8af1b1b4299668bb163b60f2447d6a9e83e8ce44
JoaquinVeraOrtega <j.veraortega99@gmail.com>
2022-11-04 17:13:56 -0300

    creando componente Persona

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BarraDeNavegacionComponent } from './components/barra-de-navegacion/barra-de-navegacion.component';
 import { ImgBackgroundComponent } from './components/img-background/img-background.component';
+import { PersonaComponent } from './components/persona/persona.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BarraDeNavegacionComponent,
-    ImgBackgroundComponent
+    ImgBackgroundComponent,
+    PersonaComponent
   ],
   imports: [
     BrowserModule
