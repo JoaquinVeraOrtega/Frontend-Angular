3b75c5b905aad30d0a195c3e3698ea29045578a7
JoaquinVeraOrtega <j.veraortega99@gmail.com>
2022-11-04 17:29:12 -0300

    creando componente skills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { BarraDeNavegacionComponent } from './components/barra-de-navegacion/barra-de-navegacion.component';
 import { ImgBackgroundComponent } from './components/img-background/img-background.component';
 import { PersonaComponent } from './components/persona/persona.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BarraDeNavegacionComponent,
     ImgBackgroundComponent,
-    PersonaComponent
+    PersonaComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule
