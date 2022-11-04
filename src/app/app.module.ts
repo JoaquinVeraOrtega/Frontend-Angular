65c4fc368c6e00066ff04b4b8caa28c06be7ef5c
JoaquinVeraOrtega <j.veraortega99@gmail.com>
2022-11-04 14:03:35 -0300

    Creando componente barra de navegacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BarraDeNavegacionComponent } from './components/barra-de-navegacion/barra-de-navegacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraDeNavegacionComponent
   ],
   imports: [
     BrowserModule
